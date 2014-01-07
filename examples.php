22b6db647932a80bbfe31c9d4dbdcdf784dfdad4
Jacob Emerick <jpemeric@gmail.com>
2014-01-07 08:05:18 -0600

    adding category fetch example, cleaning up some nasty

--- examples.php
+++ examples.php
@@ -2,22 +2,22 @@
 
 $account = 0; // your account id here
 
-include 'src/service/SearchService.php';
-
 // example - do a search for pictures of waterfalls
+include 'src/service/SearchService.php';
 $request = new BigstockAPI\Service\SearchService($account);
 $request->addTerm('waterfalls');
 $request->excludeTerm('vista');
 $request->setOrder('new');
 $request->setIllustrationConstraint('n');
-$request->setVectorConstraint('n');
 $request->setSizeConstraint('l');
+$request->setLimit(20);
 $response = $request->fetchJSON();
 
 $json = json_decode($response);
-if ($json->response_code == '200' && $json->message == 'success') {
+if ($json->response_code == 200 && $json->message == 'success') {
+    echo '<h1>Image Search Result</h1>';
     echo '<ul>';
-    foreach($json->data->images as $image) {
+    foreach ($json->data->images as $image) {
         $url = $image->small_thumb->url;
         $height = $image->small_thumb->height;
         $width = $image->small_thumb->width;
@@ -25,6 +25,21 @@ if ($json->response_code == '200' && $json->message == 'success') {
         $title = $image->title;
         
         echo "<li><img src=\"{$url}\" alt=\"{$title}\" height=\"{$height}\" width=\"{$width}\" /> - {$title}</li>";
+    }
+    echo '</ul>';
+}
+
+// example - fetch all categories
+include 'src/service/CategoryService.php';
+$request = new BigstockAPI\Service\CategoryService($account);
+$response = $request->fetchJSON();
+
+$json = json_decode($response);
+if ($json->response_code == 200 && $json->message == 'success') {
+    echo '<h1>Category Fetch Result</h1>';
+    echo '<ul>';
+    foreach ($json->data as $category) {
+        echo "<li>{$category->name}</li>";
     }
     echo '</ul>';
 }
